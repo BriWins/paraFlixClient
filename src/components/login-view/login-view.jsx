96e2cf71b880d8f26c5289d1373a4a859e63cf64
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-26 17:43:20 -0600

    requiring username and pass

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -30,12 +30,14 @@ export const LoginView = ( {onLoggedIn} ) => {
             <label> Username: 
                 <input type="text"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)} />
+                onChange={(e) => setUsername(e.target.value)}
+                required />
             </label>
             <label> Password:
                 <input type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
+                onChange={(e) => setPassword(e.target.value)}
+                required />
             </label>
             <button type="submit">Submit</button>
         </form>
