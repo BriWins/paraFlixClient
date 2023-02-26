3b5d3dc67574aeba80d01c07372fb2a44409d62b
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-26 17:07:32 -0600

    Created LoginView user state
    Added comments for clarity

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
+    const [ selectedMovie, setSelectedMovie ] = useState(null);
+    const [ user, setUser ] = useState(null);
 
-
+/* Retrieves database data through deployed app*/
     useEffect(() => {
         fetch(`https://paraflix.herokuapp.com/movies`)
         .then((response) => response.json())
@@ -21,14 +24,18 @@ export const MainView = () => {
             setMovies(moviesFromApi);
         });
     }, []);
+       
+/* Return user login form if no user is authenticated */
+    if (!user) {
+        return <LoginView />
+    }
 
-    
-     const [ selectedMovie, setSelectedMovie ] = useState(null);
-
+/* Displays all details for a single movie upon click*/
     if (selectedMovie) {
         return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     }
 
+/* No movies are returned if list is empty*/
     if (movies.length === 0) {
         return <div>The list is empty!</div>
     }
