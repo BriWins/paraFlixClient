33702376fcd52728a2fefda2ce353aa824258bb9
BriWins <95152254+BriWins@users.noreply.github.com>
2023-03-05 18:54:50 -0600

    Implement Logic
    Implented MovieView and NavigationBar logic

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,15 +1,28 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const NavigationBar = ({ user, onLoggedOut}) => {
+export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
         <Navbar bg="light" expland="lg">
             <Container>
                 <Navbar.Brand as={Link} to="/"> paraFlix</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-
+                    <Nav className="me-auto"> 
+                    {!user && (
+                        <>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/profile"><span>{user}</span></Nav.Link>
+                            <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                        </>
+                    )}
+                    
                     </Nav>
                 </Navbar.Collapse>
             </Container>
