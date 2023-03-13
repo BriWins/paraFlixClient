5fb55b6233304579818a95c4cfa2b3c857c008d9
BriWins <95152254+BriWins@users.noreply.github.com>
2023-03-12 19:51:53 -0500

    Imported two view into ProfileView

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { FavoriteMoviesView } from "../favorite-movies-view/favorite-movies-view";
+import { UserView } from "../user-view/user-view";
 
 
 export const ProfileView = ({ user }) => {
@@ -24,7 +26,8 @@ const getUser = (token) => {
 
     return (
         <Container>
-
+            <UserView token={token} user={user} />
+            <FavoriteMoviesView userFavMovies={favoriteMovies} />
         </Container>
     );
 };
